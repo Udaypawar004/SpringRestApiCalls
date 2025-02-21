8455ca6ab65a9a1f59f0a796b3df3eb48e24c932
Uday Deshmukh <udaypawar004@gmail.com>
2025-02-21 16:17:24 +0530

    [Uday] Add Uc1 Hello program

---
+++ src/main/java/com/rest/apis/HttpMethods.java
@@ -0,0 +1,15 @@
+package com.rest.apis;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class HttpMethods {
+    // Method to show hello
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from BridgeLabz";
+    }
+}
