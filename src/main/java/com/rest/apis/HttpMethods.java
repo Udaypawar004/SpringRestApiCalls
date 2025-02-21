2f59793220f324605f7daa365de320fcdf53dbdd
Uday Deshmukh <udaypawar004@gmail.com>
2025-02-21 16:33:23 +0530

    [Uday] Add Uc2 request params

--- src/main/java/com/rest/apis/HttpMethods.java
+++ src/main/java/com/rest/apis/HttpMethods.java
@@ -2,6 +2,7 @@ package com.rest.apis;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,11 @@ public class HttpMethods {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+
+    // method for request params
+    @GetMapping("/hello/query")
+    public String sayMyName(@RequestParam String name){
+        return "hello! " + name ;
     }
 }
